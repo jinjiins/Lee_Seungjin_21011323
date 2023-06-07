eda8375706f9486c2394a7b57ff94386d40f0929
user <user@localhost>
2023-06-07 20:41:21 +0900

    6/7

--- ECI2ECEF_DCM.m
+++ ECI2ECEF_DCM.m
@@ -5,12 +5,12 @@
 function DCM = ECI2ECEF_DCM(time)
 
 
-UTCdate = juliandate(time);
+UTCdate = juliandate(datetime(time));
 
 
 %Greenwich mean sidereal time
-theta = siderealTime(UTCdate);
+theta = siderealTime(UTCdate) * pi / 180;
 
-DCM = [cos(theta) -sin(theta) 0; sin(theta) cos(theta) 0; 0 0 1];
+DCM = [cos(theta) sin(theta) 0; -sin(theta) cos(theta) 0; 0 0 1];
 
 end
