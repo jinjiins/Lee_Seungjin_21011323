70caf30249767a3a8d7bc2bde7e55337095d0b45
Windows10 <Windows10@DESKTOP-8UN58QM>
2023-06-15 22:03:06 +0900

    ECEF2ENU

--- ECEF2ENU_DCM.m
+++ ECEF2ENU_DCM.m
@@ -1,6 +1,7 @@
 %Test
-function DCM = ECEF2ENU_DCM(time)
+function DCM = ECEF2ENU_DCM(lat, lon)
 
-DCM = [];
+
+DCM = [cosd(90+lat) sind(90+lat) 0; -sind(90+lat) cosd(90+ lat) 0; 0 0 1;] * [1 0 0; 0 cosd(90 - lon) sind(90 - lon); 0 sind(90 - lon) cosd(90 - lon)];
 
 end
